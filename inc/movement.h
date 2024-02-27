f5c0e30688cd4c467f25a6b2ee06a884763c64c3
oyaryza <oyaryza@stud.khpi.ucode-connect.study>
2024-02-27 22:22:39 +0200

    movement with the jump on the platforms

---
+++ inc/movement.h
@@ -0,0 +1,22 @@
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
+#include <raylib.h>
+
+typedef struct {
+    float speedX;      //скорость персонажа по горизонтали
+    float speedY;      //скорость прыжка
+    int screenWidth;   //
+    int screenHeight;  //
+    float jumpForce;   //сила прыжка
+    float gravity;
+} SizeData;
+
+
+
+bool isOnGround(Rectangle *position, const int floorPosition);          //проверка на то, стоит ли персонаж на земле
+bool isonPlatform(Rectangle *position, Rectangle *obstacle);            //проверка на то, стоит ли персонаж на земле
+void Jump(Rectangle *position, SizeData *stock, Rectangle *obstacle);   //Прыжок + проверка на колизию(платформы)
+void movement(Rectangle *position, SizeData *stock);                    //движение(передвижение по горизонтали)
+
+#endif
