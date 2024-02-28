764fdfe509142596a203ca1d03a839795f2b0a22
oyaryza <oyaryza@stud.khpi.ucode-connect.study>
2024-02-29 00:47:50 +0200

    main level: init, draw, update, unload

--- src/unload_main_level.c
+++
@@ -1,7 +0,0 @@
-#include "../inc/main_level.h"
-
-void unload_main_level(void) {
-    UnloadTexture(main_bg);
-    UnloadTexture(characterTexture);
-    UnloadTexture(platformTexture);
-}
--- src/update_main_level.c
+++
@@ -1,8 +0,0 @@
-#include "../inc/main_level.h"
-
-void update_main_level(void) {
-    movement(&characer, &stock);
-    for (int i = 0; i < NUM_OF_PLATFORMS; ++i) {
-        Jump(&characer, &stock, &platforms[i]);
-    }
-}
